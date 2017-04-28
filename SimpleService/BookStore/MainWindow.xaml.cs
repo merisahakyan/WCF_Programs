967a5d324a73b86779fe59c9d0921a21558a9b7f
merisahakyan <sahakyan_m@bk.ru>
2017-04-28 17:52:10 +0400

    Adding and removing from card

--- SimpleService/BookStore/MainWindow.xaml.cs
+++ SimpleService/BookStore/MainWindow.xaml.cs
@@ -25,15 +25,16 @@ namespace BookStore
         {
             InitializeComponent();
         }
-
-        StackPanel sp;
-        Label label;
-        Button btn;
+        static List<Book> oncard = new List<Book>();
+        int id;
+        StackPanel sp, sp_forcard, remove_sp;
+        Label label_books, label;
+        Button add_btn, Remove_btn;
         private async void Search_Button_Click(object sender, RoutedEventArgs e)
         {
             BookShopClient proxy = new BookShopClient();
             var list = await proxy.GetBooksAsync();
-
+            books_listbox.Items.Remove(sp);
 
             switch (combo.SelectedIndex)
             {
@@ -60,16 +61,55 @@ namespace BookStore
                 sp = new StackPanel();
                 sp.Orientation = Orientation.Horizontal;
 
-                label = new Label();
-                label.Content = $"{m.Name}  {m.Author}  {m.Genre}  {m.Price}$";
+                label_books = new Label();
+                label_books.Content = $"{m.Name}  {m.Author}  {m.Genre}  {m.Price}$";
+                sp.Children.Add(label_books);
 
-                btn = new Button();
-                btn.Content = "Add to cart";
+                add_btn = new Button();
+                add_btn.Name = "i" + Convert.ToString(m.ID);
+                add_btn.Click += (s, ea) =>
+                {
+                    id = int.Parse((s as Button).Name.Trim('i'));
 
-                sp.Children.Add(label);
-                sp.Children.Add(btn);
+                    foreach (var book in list)
+                    {
+                        if (book.ID == id && book.Quantity > 0)
+                        {
+                            book.Quantity--;
+                            books_listbox.Items.Refresh();
+                            oncard.Add(book);
 
-                listbox.Items.Add(sp);
+                            sp_forcard = new StackPanel();
+                            sp_forcard.Orientation = Orientation.Horizontal;
+                            sp_forcard.Name = "i" + book.ID.ToString();
+
+                            label = new Label();
+                            label.Content = $"{book.Name}";
+
+                            Remove_btn = new Button();
+                            Remove_btn.Content = "Remove";
+                            Remove_btn.Name = "i" + card_listbox.Items.Count.ToString();
+                            Remove_btn.Click += (se, ear) =>
+                              {
+                                  book.Quantity++;
+                                  card_listbox.Items.RemoveAt(int.Parse((se as Button).Name.Trim('i')));
+                              };
+
+                            sp_forcard.Children.Add(label);
+                            sp_forcard.Children.Add(Remove_btn);
+
+                            card_listbox.Items.Add(sp_forcard);
+                            break;
+                        }
+                    }
+
+                };
+                add_btn.Content = "Add to cart";
+
+
+                sp.Children.Add(add_btn);
+
+                books_listbox.Items.Add(sp);
             }
 
         }
