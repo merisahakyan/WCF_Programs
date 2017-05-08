60746810c0bb54d6f4ab703617d1dd78aa610964
merisahakyan <sahakyan_m@bk.ru>
2017-05-08 16:47:38 +0400

    support chat

---
+++ Support/ChatService/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Support;
+using System.ServiceModel;
+
+namespace ChatService
+{
+    public partial class MainWindow : Window, IMessage
+    {
+        public ServiceHost host = null;
+        SupportServiceContext _context = new SupportServiceContext();
+        List<Company> companies = new List<Company>();
+        List<Messaging> messagess = new List<Messaging>();
+        int compID = -1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sendbutton.IsEnabled = false;
+            host = new ServiceHost(typeof(MainWindow), new Uri("net.tcp://localhost:7000"));
+            host.AddServiceEndpoint(typeof(IMessage), new NetTcpBinding(), "");
+            host.Open();
+            GetCompanies();
+            foreach (var m in companies)
+            {
+                ListBoxItem newitem = new ListBoxItem();
+                newitem.Content = m.CompanyName;
+                newitem.Name = "i" + m.CompanyID;
+                newitem.MouseDoubleClick += (sender, e) =>
+                {
+                    messages.Text = "";
+                    companyname.Content = m.CompanyName;
+                    sendbutton.IsEnabled = true;
+                    GetMessages(m.CompanyID);
+                    foreach (var n in messagess)
+                    {
+                        messages.Text += n.Sender + "-->" + n.MessageText + System.Environment.NewLine;
+                    }
+                    compID = int.Parse((sender as ListBoxItem).Name.Trim('i'));
+                };
+                companieslistbox.Items.Add(newitem);
+            }
+        }
+
+        void GetCompanies()
+        {
+            companies = _context.Companies.ToList();
+        }
+        void GetMessages(int companyID)
+        {
+            messagess = _context.Messagings.Where((m) => (m.CompanyID == companyID)).Select((s) => (s)).ToList();
+        }
+
+
+        public void AddMessage(string message, string sender, int userid, int companyid)
+        {
+            Messaging mess = new Messaging();
+
+            mess.MessageText = message;
+            mess.Sender = sender;
+            mess.UserID = userid;
+            mess.CompanyID = companyid;
+            mess.Time = DateTime.Now;
+
+
+            _context.Messagings.Add(mess);
+            _context.SaveChanges();
+        }
+
+        
+
+        private void sendbutton_Click(object sender, RoutedEventArgs e)
+        {
+            AddMessage(message.Text, "Meri", 1, compID);
+            messages.Text += "Meri" + "-->" + message.Text + System.Environment.NewLine;
+            message.Text = "";
+        }
+    }
+}
---
+++ Support/Support/IMessageCallBack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Support
+{
+    [ServiceContract]
+    public interface IMessageCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void OnMessageAdded(string message, DateTime timestamp);
+    }
+
+    [ServiceContract(CallbackContract = typeof(IMessageCallback), SessionMode = SessionMode.Required)]
+    public interface IMessage
+    {
+        [OperationContract(IsOneWay = true)]
+        void AddMessage(string message, string sender, int userid, int companyid);
+
+
+    }
+}
